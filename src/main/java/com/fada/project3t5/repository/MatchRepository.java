46e02a02e58464abd48865a5c0237919941cd1e3
fadamaka <tamas.hercz@cubicfox.com>
2022-06-12 19:59:56 +0200

    deleted old files

--- src/main/java/com/fada/project3t5/repository/MatchRepository.java
+++
@@ -1,8 +0,0 @@
-package com.fada.project3t5.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.fada.project3t5.domain.Match;
-
-public interface MatchRepository extends JpaRepository<Match,Integer> {
-}
--- src/main/java/com/fada/project3t5/repository/PlayerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.fada.project3t5.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.fada.project3t5.domain.Player;
-
-public interface PlayerRepository extends JpaRepository<Player,Integer> {
-}
